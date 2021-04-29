0316e3ff40786896495733a05a48c57b63f94b4e
Femi <ologunwafemi@gmail.com>
2021-04-29 16:50:59 +0100

    setting up static look

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 
 function App() {
    return (
-      <div className='App'>
-         <h1>Birthday Project</h1>
-      </div>
+      <main>
+         <section className='container'>
+            <h3>0 birthdays today</h3>
+            <List />
+            <button onClick={() => console.log('you clicked me')}>
+               clear all
+            </button>
+         </section>
+      </main>
    );
 }
 
