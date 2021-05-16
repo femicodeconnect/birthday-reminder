09b6fad0414c08ea014656e518c0aab32030eb5d
Femi <femiconnect234@gmail.com>
2021-05-16 07:00:43 +0100

    Updates

---
+++ precache-manifest.94f834b9ddee8e7fea359de3c63aebba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de63b065af2dd791410534fcad01272",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "b233fb213f6937b054b9",
+    "url": "/birthday-reminder/static/css/main.5f6885b7.chunk.css"
+  },
+  {
+    "revision": "3bdc1d8518940277ff32",
+    "url": "/birthday-reminder/static/js/2.ab0cd556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.ab0cd556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b233fb213f6937b054b9",
+    "url": "/birthday-reminder/static/js/main.ed7ca3b1.chunk.js"
+  },
+  {
+    "revision": "a1ff553ac3d675eebff3",
+    "url": "/birthday-reminder/static/js/runtime-main.ed31b697.js"
+  }
+]);
